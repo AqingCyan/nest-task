164b1c11bcb31aff8d011d16d11fddcd82c97503
AqingCyan <aqingcyan@cyanthing.com>
2021-10-27 23:02:22 +0800

    feat: 尝试计划任务开发

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,52 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { CronExpression, SchedulerRegistry } from '@nestjs/schedule';
+import { CronJob } from 'cron';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private schedulerRegistry: SchedulerRegistry,
+  ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('/cron/:action')
+  manageCron(@Param('action') action: string) {
+    const greet = this.schedulerRegistry.getCronJob('greet');
+
+    if (action === 'info') {
+      console.log('上一次执行', greet.lastDate());
+      console.log('下一次执行', greet.nextDates(5));
+    }
+
+    if (action === 'stop') {
+      greet.stop();
+    }
+
+    if (action === 'start') {
+      greet.start();
+    }
+  }
+
+  @Post('/cron')
+  addCron(@Body() body: any) {
+    const { name } = body;
+    const job = new CronJob(CronExpression.EVERY_SECOND, () => {
+      console.log('你好', name);
+    });
+
+    this.schedulerRegistry.addCronJob(name, job);
+
+    job.start();
+  }
+
+  @Delete('/cron/:name')
+  deleteCron(@Param('name') name: string) {
+    this.schedulerRegistry.deleteCronJob(name);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { Cron, CronExpression, Interval, Timeout } from '@nestjs/schedule';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+
+  /**
+   * 计划任务执行
+   * @Cron('* * * * * *') // 秒 分 时 天 月 年
+   */
+  @Cron(CronExpression.EVERY_5_SECONDS, { name: 'greet' })
+  handleCron() {
+    console.log('Hello');
+  }
+
+  @Interval(1000)
+  handleInterval() {
+    // console.log('滴答');
+  }
+
+  @Timeout(3000)
+  handleTimeout() {
+    // console.log('咯嘣');
   }
 }
