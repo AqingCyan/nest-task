b21288bbf5799a93b6a54e67677597871a5f4c0c
AqingCyan <aqingcyan@cyanthing.com>
2021-10-31 23:38:25 +0800

    feat: 任务队列监听

--- src/app.processor.ts
+++ src/app.processor.ts
@@ -1,10 +1,26 @@
-import { Process, Processor } from '@nestjs/bull';
-import { Job } from 'bull';
+import {
+  OnQueueActive,
+  OnQueueCompleted,
+  Process,
+  Processor,
+} from '@nestjs/bull';
+import { DoneCallback, Job } from 'bull';
 
 @Processor('app')
 export class AppProcessor {
   @Process('resizeImage')
-  processResizeImage(job: Job) {
+  processResizeImage(job: Job, done: DoneCallback) {
     console.log(`处理任务：缩放图像文件 ${job.data.file}`);
+    done(null, '成功～～');
+  }
+
+  @OnQueueActive()
+  onActive(job: Job) {
+    console.log(`任务活动：正在处理任务 ${job.name} (${job.id}) ...`);
+  }
+
+  @OnQueueCompleted()
+  onCompleted(job: Job, result: any) {
+    console.log(`任务完成 ${job.name} (${job.id})，${result}`);
   }
 }
